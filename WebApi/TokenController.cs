6d4b7a1d7bd140749776ddf3aa734b286dae9cf3
Jimmy Romero <jerm.pcd@hotmail.com>
2025-10-21 20:57:48 -0500

    Se realizo para verificar el token

---
+++ WebApi/TokenController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiSchool.WebApi
+{
+    [Authorize]
+    [RoutePrefix("api/token")]
+    public class TokenController : ApiController
+    {
+        [HttpGet]
+        [Route("check-status")]
+        public HttpResponseMessage checkStatus()
+        {
+            
+            return Request.CreateResponse(HttpStatusCode.OK, true);
+        }
+    }
+}
