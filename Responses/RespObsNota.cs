2c10c6199e60c6c3ba3a5135d4996ee2b5616aa1
devRomerito <jerm.pcd@gmail.com>
2026-09-06 21:52:10 -0500

    Se agrega cambios de tipo bool, se agrega la cadena de conexion y objetos

--- Responses/RespObsNota.cs
+++ Responses/RespObsNota.cs
@@ -5,7 +5,7 @@
         public int idRegOb { get; set; }
         public string tipoObs { get; set; }
         public string observacion { get; set; }
-        public bool cumplido { get; set; }
+        public bool? cumplido { get; set; }
         public string comentarioAd { get; set; }
         public string estado { get; set; }
     }
