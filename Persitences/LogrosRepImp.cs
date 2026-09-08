2330441f93a8db0c0c8430be224f642d02b48ef8
devRomerito <jerm.pcd@gmail.com>
2026-09-08 18:14:43 -0500

    Se quita el usuario de la busquea del logro

--- Persitences/LogrosRepImp.cs
+++ Persitences/LogrosRepImp.cs
@@ -22,14 +22,14 @@ namespace WebApiSchool.Persitences
             sqlGetLogrosByUserGradoAsignPer.AppendLine($"WHERE codAsignatura = '{findLogros.codAsignatura}' ");
             sqlGetLogrosByUserGradoAsignPer.AppendLine($"AND codGrado = '{findLogros.codGrado}' ");
             sqlGetLogrosByUserGradoAsignPer.AppendLine($"AND periodo = '{findLogros.periodo}' ");
-            sqlGetLogrosByUserGradoAsignPer.AppendLine($"AND usuario = '{findLogros.usuario}'");
+            //sqlGetLogrosByUserGradoAsignPer.AppendLine($"AND usuario = '{findLogros.usuario}'");
 
             return _context.Database.SqlQuery<LogroEntity>(sqlGetLogrosByUserGradoAsignPer.ToString()).ToList();
         }
 
         public int SaveLogros(ReqSaveLogros saveLogros)
         {
-                StringBuilder sqlSaveLogros = new StringBuilder($"insert into B3Indicadores values (dbo.Fnc_Defano(), ");
+            StringBuilder sqlSaveLogros = new StringBuilder($"insert into B3Indicadores values (dbo.Fnc_Defano(), ");
             sqlSaveLogros.AppendLine($" '0', '{saveLogros.descLogro}', {saveLogros.periodo}, {saveLogros.cantNotas}, {saveLogros.pc1}, ");
             sqlSaveLogros.AppendLine($"{saveLogros.pc2}, {saveLogros.pc3}, {saveLogros.pc4}, 1, 1, 1, 0, '{saveLogros.usuario}', 'Web', getDate(), ");
             sqlSaveLogros.AppendLine($"'{saveLogros.codGrado}', '{saveLogros.codAsignatura}', dbo.fnc_get_max_consecut() ) ");
